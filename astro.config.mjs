bf2e4bc6762a3fc2e88b53a6a2f3a41151d92738
Ahmed Fasih <wuzzyview@gmail.com>
2023-04-09 22:11:13 -0500

    GitHub pages URL shenanigans

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  site: 'https://fasiha.github.io',
+  base: '/kamiya-codec',
 });
